8e496d863b2b00fb5f63033148fc8daab7554a04
Andrei <example@gmail.com>
2021-09-03 11:52:45 +0200

    second commit

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
 "use strict";
 
-let question = confirm("How are You?");
-console.log(question);
+// let question = confirm("How are You?");
+// console.log(question);
+
+var numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?");
